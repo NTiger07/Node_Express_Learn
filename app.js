c43c9894462d5b8988fc1ec1a3238b070e6d37d7
NTiger07 <falaleru@gmail.com>
2022-06-14 13:39:57 +0100

    Idek sef

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 let { people } = require("./data");
 
+app.use(express.static("./methods-public"))
+
+app.use(express.urlencoded({ extended: false })) //to parse form data
 app.get("/api/people", (req, res) => {
   res.status(200).json(people);
 });
-
+app.post("/login", (req, res) => {
+  const { name } = req.body
+  if (name) {
+    res.status(200).send(`Welcome ${name}`)
+  }
+  if (!name) {
+    res.status(401).send("Please provide credentials")
+  }
+})
 app.listen(5000, () => {
   console.log("Server listening on port 5000...");
 });
