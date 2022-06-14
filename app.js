eb2f6c2381491546e0a971543962231fad4083c1
NTiger07 <falaleru@gmail.com>
2022-06-14 14:56:54 +0100

    sped up loading time and fixed name not appearing bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 let { people } = require("./data");
 
 app.use(express.static("./methods-public"))
+app.use(express.json())
 
 app.use(express.urlencoded({ extended: false })) //to parse form data
 app.get("/api/people", (req, res) => {
@@ -11,11 +12,21 @@ app.get("/api/people", (req, res) => {
 app.post("/login", (req, res) => {
   const { name } = req.body
   if (name) {
-    res.status(200).send(`Welcome ${name}`)
+    res.status(200).send(person.name)
   }
   if (!name) {
     res.status(401).send("Please provide credentials")
   }
+})
+app.post("/api/people", (req, res) => {
+  const { name } = req.body
+  if (name) {
+    res.status(201).send(name)
+  }
+  if (!name) {
+    res.status(400).send("Please provide name value")
+  }
+  // res.status(201).send("Success")
 })
 app.listen(5000, () => {
   console.log("Server listening on port 5000...");
